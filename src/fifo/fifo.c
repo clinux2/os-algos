5dc5beaa31b351c1e7fadd010fb27f965936892d
debian <debian@192.168.1.21.non-exists.ptr.local>
2025-05-09 13:09:47 +0700

    Put .c in the right folder

---
+++ src/fifo/fifo.c
@@ -0,0 +1,58 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+
+int fifo(int* pages, int numpages, int numframes);
+bool isIn(int* frames, int numframes, int page);
+void display(int* frames, int numframes, int hand, int pagefault);
+
+int main()
+{
+	int numpages = 20, numframes = 3;
+	int pages[] = {7,0,1,2,0,3,0,4,2,3,0,3,2,1,2,0,1,7,0,1};
+	fifo(pages, numpages,  numframes);
+
+}
+
+int fifo(int* pages, int numpages, int numframes)
+{
+	int *frames = (int*)malloc(numframes * sizeof(int));
+	int hand = 0, pagefault = 0;
+	bool is_in;
+	
+	for (int i=0; i<numframes; i++) frames[i] = -1;
+
+
+	for (int i=0; i<numpages; i++)
+	{
+		
+		//If already in memory, skip
+		is_in = isIn(frames, numframes, pages[i]);
+		if (is_in) continue;
+		
+		//Page fault
+		pagefault++;
+		frames[hand] = pages[i];
+		hand++;
+		hand = hand % numframes;
+		
+		display(frames, numframes, hand, pagefault);	
+	}
+	return pagefault;
+}
+
+bool isIn(int* frames, int numframes, int page)
+{
+	for (int i=0; i<numframes; i++) if (frames[i] == page) return true;
+	return false; 
+}
+
+void display(int* frames, int numframes, int hand, int pagefault)
+{
+	
+	for (int i=0; i<numframes; i++)
+		printf("%d ", frames[i]);
+	printf("\n");
+	printf(" hand: %d\n", hand);
+	printf("pagefault: %d\n\n", pagefault);
+}
---
+++ src/lru/lru.c
@@ -0,0 +1,71 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+
+int lru(int* pages, int numpages, int numframes);
+void display(int* frames, int numframes, int pagefault);
+int isIn(int* frames, int numframes, int page);
+int min(int *arr, int size);
+
+int main()
+{
+	int numpages = 20, numframes = 3;
+	int pages[] = {7,0,1,2,0,3,0,4,2,3,0,3,2,1,2,0,1,7,0,1};
+	lru(pages, numpages,  numframes);
+
+}
+
+int lru(int* pages, int numpages, int numframes)
+{
+	int *frames = (int*)malloc(numframes * sizeof(int));
+	int *lru_trackings = (int*)malloc(numframes * sizeof(int));
+	int pagefault = 0, clock = 0;
+	
+	for (int i=0; i<numframes; i++) frames[i] = -1;
+	display(frames, numframes, pagefault);
+	
+
+	int min_i, isin_i;
+	for (int i=0; i<numpages; i++) 
+	{	
+		clock++;	
+
+		//Update priority if found
+		isin_i = isIn(frames, numframes, pages[i]);
+		if (isin_i >= 0) {lru_trackings[isin_i] = clock; continue;}
+		
+		//Occur page fault
+		if (pagefault < numframes) {min_i = pagefault;}
+		else { min_i = min(lru_trackings, numframes);	}
+		frames[min_i] = pages[i]; lru_trackings[min_i] = clock;
+		
+		pagefault++;
+		printf("min_i: %d\n", min_i);
+		display(frames, numframes, pagefault);
+	}
+	
+
+	return pagefault;
+}
+
+int isIn(int* frames, int numframes, int page)
+{
+	for (int i=0; i<numframes; i++) if (frames[i] == page) return i;
+	return -1; 
+}
+void display(int* frames, int numframes, int pagefault)
+{
+	
+	for (int i=0; i<numframes; i++)
+		printf("%d ", frames[i]);
+	printf("\n");
+	printf("pagefault: %d\n", pagefault);
+}
+
+
+int min(int *arr, int size)
+{
+	int min_i = 0;
+	for (int i=0; i<size; i++) if (arr[i] < arr[min_i]) min_i = i;
+	return min_i;
+}
